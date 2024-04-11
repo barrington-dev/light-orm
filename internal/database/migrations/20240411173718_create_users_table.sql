99c4ff67f4f09b4ae7bd25965ee7b80793be4df1
Barrington Hulana <barringtonhulana.dev@gmail.com>
2024-04-11 20:01:23 +0200

    Create migration file for users table

---
+++ internal/database/migrations/20240411173718_create_users_table.sql
@@ -0,0 +1,20 @@
+-- +goose Up
+-- +goose StatementBegin
+CREATE TABLE IF NOT EXISTS users (
+     id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+     username VARCHAR(50) UNIQUE NOT NULL,
+     first_name VARCHAR(50) NOT NULL,
+     last_name VARCHAR(50) NOT NULL,
+     contact_number VARCHAR(20) NOT NULL,
+     email varchar(255) UNIQUE NOT NULL,
+     password varchar(255) NOT NULL,
+     is_admin BOOLEAN DEFAULT '0' NOT NULL,
+     created_at BIGINT NOT NULL,
+     updated_at BIGINT
+);
+-- +goose StatementEnd
+
+-- +goose Down
+-- +goose StatementBegin
+DROP TABLE IF EXISTS users;
+-- +goose StatementEnd
