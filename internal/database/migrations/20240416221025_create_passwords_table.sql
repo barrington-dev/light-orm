2364eaaffff92b39a2e2517d7893a2cd637f671a
Barrington Hulana <barringtonhulana.dev@gmail.com>
2024-05-17 21:22:21 +0200

    Update passwords table

--- internal/database/migrations/20240416221025_create_passwords_table.sql
+++ internal/database/migrations/20240416221025_create_passwords_table.sql
@@ -3,13 +3,13 @@
 CREATE TABLE IF NOT EXISTS passwords (
     id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     user_id BIGINT NOT NULL,
-    password VARCHAR(255) NOT NULL,
+    hash VARCHAR(255) NOT NULL,
     created_at TIMESTAMP NOT NULL,
     CONSTRAINT passwords_user_id_fkey FOREIGN KEY (user_id) REFERENCES users(id)
         ON DELETE CASCADE
 );
 
-CREATE INDEX IF NOT EXISTS passwords_password_idx ON passwords USING btree (password);
+CREATE INDEX IF NOT EXISTS passwords_hash_idx ON passwords USING btree (hash);
 -- +goose StatementEnd
 
 -- +goose Down
